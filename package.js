1412c7da21ab06cdeae4fb1067f5225e55fe85a0
osxi <osxi@users.noreply.github.com>
2015-08-05 10:57:58 -0500

    lock down babel version

--- package.js
+++ package.js
@@ -8,14 +8,14 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use('grigio:babel');
+  api.use('grigio:babel@0.0.14');
   api.addFiles('meteor-testing-tools.es6.js');
   api.export("MTT");
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('grigio:babel');
+  api.use('grigio:babel@0.0.14');
   // api.use('meteor-testing-tools');
   api.addFiles('meteor-testing-tools-tests.js');
 });
